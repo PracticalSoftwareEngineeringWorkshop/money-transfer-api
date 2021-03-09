1240eb9914c60f848c5a45b07268fcdabf9d586c
Biniam Asnake <biniamasnake@gmail.com>
2021-03-09 22:13:11 +0100

    Initial commit - Spring Boot project created with dependencies

---
+++ src/test/java/com/psew/moneytransfer/MoneyTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psew.moneytransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
